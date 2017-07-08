43e7f7ce4f69e28c94f6ae7deb5a2ec0444eb2f6
Konrad Blum <konrad.blum@gmail.com>
2017-07-08 14:20:29 +0200

    POST temperature readings as JSON to API endpoint.

--- README.md
+++ README.md
@@ -8,3 +8,49 @@ This project is a [NodeMCU](http://www.nodemcu.com/index_en.html) temperature se
 ## Hardware
 
 The 1-Wire sensor bus should be connected to digital I/O pin D2.
+
+
+## Configuration
+
+Create a `config.h` file and populate with the following settings (substituting in appropriate values):
+
+```cpp
+// Wi-Fi credentials
+#define WLAN_SSID "SSID"
+#define WLAN_PASS "PASSWORD"
+
+// API endpoint
+#define API_HOST "HOST"
+#define API_ENDPOINT "/api"
+#define API_PORT 80
+// empty string for no auth or base64 encoded API credentials
+#define API_AUTH_VALUE ""
+```
+
+The configuration file is not under version control.
+
+
+## API
+
+The temperature readings are sent to a remote server through an HTTP/1.1 POST request. The data is sent in JSON format.
+
+The following headers are included in the request:
+
+* `Host`
+* `Authorization` - HTTP Basic authentication (if configured)
+* `User-Agent` - `NodeMCU/1.0`
+* `Content-Type` - `application/json`
+* `Connection` - `close`
+* `Content-Length`
+
+Examples of request body:
+
+```json
+{ "readings": { "0x28ff123456789abc": 20.50 } }
+```
+
+```json
+{ "readings": { "0x28ff123456789abc": 18.06, "0x28ff123456789abd": 17.19 } }
+```
+
+The unique 64-bit serial code for each device (in hexadecimal format) is mapped to the temperature reading from the device.
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+
+#include "config.h"
+
+// amount of time in milliseconds to wait before closing connection with no data being receieved
+#define IDLE_TIMEOUT_MS 3000
 
 const int ONE_WIRE_PIN = D2;
 
@@ -69,6 +75,24 @@ void printAddress(byte address[DEVICE_ADDRESS_LENGTH]) {
   Serial.print(F(" }"));
 }
 
+/**
+ * Return the address of a device as a hexadecimal string.
+ *
+ * Example:
+ *   0x28ff123456789abc
+ */
+String formatAddress(byte address[8]) {
+   String s = "0x";
+   for (uint8_t i = 0; i < 8; i++) {
+    // zero-pad the address if necessary
+    if (address[i] < 16) {
+      s += "0";
+    }
+    s += String(address[i], HEX);
+   }
+   return s;
+}
+
 void readSensors(Reading readings[]) {
   // call sensors.requestTemperatures() to issue a global temperature request to all devices in the bus
   Serial.println(F("Requesting temperatures..."));
@@ -92,7 +116,6 @@ void readSensors(Reading readings[]) {
 }
 
 void printReadings(Reading readings[]) {
-  Serial.println(F("Readings:"));
   for (deviceIndex = 0; deviceIndex < deviceCount; deviceIndex++) {
       Reading reading = readings[deviceIndex];
       if (reading.valid) {
@@ -106,11 +129,116 @@ void printReadings(Reading readings[]) {
   }
 }
 
+void wifiConnect() {
+  Serial.print(F("Connecting to Wi-Fi network: "));
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(F("."));
+    delay(1000);
+  }
+  Serial.println(); // close off "connection dots" line
+
+  Serial.print(F("Connected to Wi-Fi network with IP address: "));
+  Serial.println(WiFi.localIP());
+}
+
+/**
+ * Construct JSON message with sensor readings.
+ *
+ * Examples:
+ *   { "readings": { "0x28ff123456789abc": 20.50 } }
+ *   { "readings": { "0x28ff123456789abc": 18.06, "0x28ff123456789abd": 17.19 } }
+ */
+String constructMessage(Reading readings[]) {
+  String message = "{ \"readings\": {";
+
+  for (deviceIndex = 0; deviceIndex < deviceCount; deviceIndex++) {
+      Reading reading = readings[deviceIndex];
+      if (reading.valid) {
+        String address = formatAddress(reading.deviceAddress);
+        // readings are comma-separated
+        message = message + " \"" + address + "\": " + String(reading.temperature) + ",";
+      }
+  }
+
+  // remove trailing "," for last reading in comma-separated list
+  if (message[message.length() - 1] == ',') {
+    message.remove(message.length() - 1);
+  }
+  message += " } }";
+
+  return message;
+}
+
+void sendReadings(Reading readings[]) {
+  String message = constructMessage(readings);
+  Serial.println(F("Sensor data message:"));
+  Serial.println(message);
+
+  WiFiClient client;
+
+  Serial.print(F("Connecting to server "));
+  Serial.print(API_HOST);
+  Serial.print(F(" on port "));
+  Serial.print(API_PORT);
+  Serial.print(F(" with endpoint "));
+  Serial.println(API_ENDPOINT);
+  if (client.connect(API_HOST, API_PORT)) {
+    // send request to server
+    client.print(F("POST "));
+    client.print(API_ENDPOINT);
+    client.println(F(" HTTP/1.1"));
+    client.print(F("Host: "));
+    client.println(API_HOST);
+    if (API_AUTH_VALUE != "") {
+      Serial.println(F("Using HTTP Basic authentication"));
+      client.print(F("Authorization: Basic "));
+      client.println(API_AUTH_VALUE);
+    } else {
+      Serial.println(F("Not using authentication"));
+    }
+    client.println(F("Content-Type: application/json"));
+    client.println(F("User-Agent: NodeMCU/1.0"));
+    client.println(F("Connection: close"));
+    client.print(F("Content-Length: "));
+    client.println(message.length(), DEC);
+    client.println();
+    client.println(message);
+    client.println();
+
+    // read response data until either the connection is closed, or the idle timeout is reached
+    Serial.println(F("-----------------------"));
+    unsigned long lastRead = millis();
+    while (client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+      while(client.available()) {
+        char c = client.read();
+        Serial.print(c);
+        lastRead = millis();
+      }
+    }
+    Serial.println(F("-----------------------"));
+  } else {
+    Serial.println(F("Unable to connect to server"));
+  }
+
+  // close connection to server
+  client.stop();
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Starting..."));
 
   sensorInit();
+
+  // set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  if (WiFi.isConnected()) {
+    WiFi.disconnect();
+  }
+  delay(100);
 
   Serial.println(F("Setup done"));
 }
@@ -120,6 +248,10 @@ void loop() {
   readSensors(readings);
 
   printReadings(readings);
+
+  wifiConnect();
+  sendReadings(readings);
+  WiFi.disconnect();
 
   delay(2000);
 }
